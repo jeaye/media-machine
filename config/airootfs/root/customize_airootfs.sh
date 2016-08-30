5d71b27239c522272c51a77094c346d1feee9c4d
jeaye <contact@jeaye.com>
2016-08-30 12:16:10 -0700

    Add initial machine setup in airootfs

--- config/airootfs/root/customize_airootfs.sh
+++ config/airootfs/root/customize_airootfs.sh
@@ -1,3 +1,9 @@
+ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+
+echo media-machine > /etc/hostname
+
+systemctl enable sshd ; systemctl start sshd
+
 for file in i3 i3status.conf xinitrc Xresources config;
 do
     cp -rf /etc/skel/$file ~/.$file
