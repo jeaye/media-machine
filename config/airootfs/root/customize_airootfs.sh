ceb082e7717c05305a0d2a153eea5fc21f218966
jeaye <contact@jeaye.com>
2016-08-31 19:47:07 -0700

    Setup locale before anything

--- config/airootfs/root/customize_airootfs.sh
+++ config/airootfs/root/customize_airootfs.sh
@@ -1,12 +1,21 @@
+### Language and region
+sed -i 's/^#\(en_US.UTF-8 UTF-8\)/\1/g' /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+export LANG=en_US.UTF-8
+
 ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 
+### System info
 echo media-machine > /etc/hostname
 
+### Services
 systemctl enable sshd ; systemctl start sshd
 
+#aticonfig --initial
+
+### User files
 for file in i3 i3status.conf xinitrc Xresources config;
 do
     cp -rf /etc/skel/$file ~/.$file
 done
-
-#aticonfig --initial
